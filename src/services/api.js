22a3c13b9c71057bd2cdce87296124584bdb18c5
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2026-04-06 12:59:37 +0530

    [Paarth Agarwal] Refactor.  the code for QMA frontend

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:"quantitymeasurementapp-production-db23.up.railway.app"
+  baseURL:"https://quantitymeasurementapp-production-db23.up.railway.app"
 });
 
 api.interceptors.request.use((config) => {
